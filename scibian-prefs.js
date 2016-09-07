68266070e87e15ed72d970c1f66b3beb4da8fe19
Scibian Packaging Team <packaging@scibian.org>
2016-09-07 11:34:37 +0200

    Initial release

---
+++ scibian-prefs.js
@@ -0,0 +1,6 @@
+// Calibre configuration file
+pref("general.config.obscure_value", 0);
+pref("general.config.filename", "calibre.cfg");
+
+// Disable add-on control wizard
+pref("extensions.shownSelectionUI", true);
